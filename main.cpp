6d3392335d0cbc1d0a639bf7bfa2222459168dc6
Wojtek <wojciech.lawnikowicz@gmail.com>
2023-03-18 13:58:01 +0100

    dodana funkcja gra

--- main.cpp
+++ main.cpp
@@ -2,17 +2,31 @@
 #include <fstream>
 #include <string>
 #include <map>
-
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main(){
+void gra(mapa, lista)
+{
+    srand( time( NULL ) );
+    int liczby=rand()%(lista.size());
+    for(int i=0; i<)
+    {
+
+    }
+}
+int main()
+{
     ifstream infile;
     infile.open("fiszki.txt");
     map <string, string> slownik;
+    vector <string>lista;
     string slowo1, slowo2;
     char znak;
     while(infile>>slowo1>>znak>>slowo2){
         slownik[slowo1] = slowo2;
+        lista.push (slowo1);
     }
     return 0;
 }
