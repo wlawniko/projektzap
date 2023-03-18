e06c00ac8a44bf7714cc5de5e3500326a3890d2d
sryszka <01178585@pw.edu.pl>
2023-03-18 14:11:47 +0100

    wstepna wersja gry

--- main.cpp
+++ main.cpp
@@ -7,14 +7,52 @@
 #include <ctime>
 using namespace std;
 
-void gra(mapa, lista)
+void gra(map<string, string>mapa, vector<string>lista)
 {
     srand( time( NULL ) );
-    int liczby=rand()%(lista.size());
-    for(int i=0; i<)
+    int wylosowane[10];
+    for(int i = 0; i < 10; i++)
     {
-
+        wylosowane[i] = -1;
     }
+    for(int i=0; i<10; i++)
+    {
+        int liczba=rand()%(lista.size());
+        int czybylo = 0;
+        for(int j = 0; j < 10; j++)
+        {
+            if(wylosowane[j] == liczba)
+            {
+                czybylo = 1;
+                j = 10;
+            }
+        }
+        if(czybylo == 1)
+        {
+            i--;
+        }
+        else{
+            wylosowane[i] = liczba;
+        }
+    }
+    cout<<"Podaj angielskie tlumaczenie tych slow: \n";
+    string odpowiedz, slowo;
+    int dobre = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        slowo = lista[wylosowane[i]];
+        cout<<i+1<<". "<<slowo<<" - ";
+        cin>>odpowiedz;
+        if(odpowiedz == mapa[slowo])
+        {
+            cout<<"dobrze\n";
+            dobre++;
+        }
+        else{
+            cout<<"zle\n";
+        }
+    }
+    cout<<"Udalo ci sie odgadnac "<<dobre<<"/10\n";
 }
 int main()
 {
@@ -26,7 +64,8 @@ int main()
     char znak;
     while(infile>>slowo1>>znak>>slowo2){
         slownik[slowo1] = slowo2;
-        lista.push (slowo1);
+        lista.push_back(slowo1);
     }
+    gra(slownik, lista);
     return 0;
 }
