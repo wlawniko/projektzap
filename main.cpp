47e0fccdbff74ba97b18ac7bd54f92f7d22d1511
sryszka <01178585@pw.edu.pl>
2023-03-18 13:42:19 +0100

    ladoawnie fiszek z pliku do mapy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
 
 int main(){
-
+    ifstream infile;
+    infile.open("fiszki.txt");
+    map <string, string> slownik;
+    string slowo1, slowo2;
+    char znak;
+    while(infile>>slowo1>>znak>>slowo2){
+        slownik[slowo1] = slowo2;
+    }
     return 0;
 }
